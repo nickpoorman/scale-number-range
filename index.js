222d622560cff7613f5571bba69373e5f5d0d041
Nick Poorman <nickpoorman@gmail.com>
2015-06-03 16:28:40 -0400

    added debug, env var to throw errors

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 /**
  * simple module to scale a number from one range to another
  */
+var debug = require('debug')('scale-number-range');
 
 module.exports = function scaleNumberRange(number, oldMin, oldMax, newMin, newMax) {
+  if (process.env.SCALE_THROW_OOB_ERRORS) {
+    if (number < oldMin) {
+      debug('ERROR OOB - scale(%d, %d, %d, %d, %d)', number, oldMin, oldMax, newMin, newMax);
+      throw new Error('number is less than oldMin');
+    }
+    if (number > oldMax) {
+      debug('ERROR OOB - scale(%d, %d, %d, %d, %d)', number, oldMin, oldMax, newMin, newMax);
+      throw new Error('number is less than oldMin');
+    }
+  }
   return (((newMax - newMin) * (number - oldMin)) / (oldMax - oldMin)) + newMin;
 }
